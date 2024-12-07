03d880fe2de4420f9c705a4c260459447cf80dad
aloHyomora <alohomoratolit@naver.com>
2024-12-07 14:56:34 +0900

    [Feat] : Player 입력 위치에 따른 캐릭터 이동 로직

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,34 +11,58 @@ public class PlayerController : MonoBehaviour
     
     public Transform player; 
     public Image playerImage;
-    
     public List<Sprite> sprites; // 스프라이트 리스트 (0번: 기본, 나머지: 변경용)
     public Coroutine currentCoroutine; // 현재 실행 중인 코루틴을 추적
+
+    [SerializeField] private float moveSpeed = 500f;
 
     private void Awake()
     {
         if(Instance == null) Instance = this;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        /*QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 10;*/
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (!GameManager.Instance.isPaused)
-        {
+        if(!GameManager.Instance.isPaused)
+        {            
             // TODO: 태블릿 Touch Input을 Canvas 상의 위치로 반영
-            float horizontal = Input.GetAxis("Horizontal");
+            if(Input.touchCount > 0){
+                Touch touch = Input.GetTouch(0);
+                HandleTouch(touch.position);
+            }
 
-            Vector2 position = player.position;
-            position.x = position.x + 500f * horizontal * Time.deltaTime;
-            player.position = position;
+            // 에디터나 PC에서 테스트를 위한 마우스 입력 지원
+            #if UNITY_EDITOR || UNITY_STANDALONE
+            else if(Input.mousePresent && Input.GetMouseButton(0))
+            {
+                HandleTouch(Input.mousePosition);
+            }
+            #endif
         }
+    }
+
+    private void HandleTouch(Vector2 inputPosition)
+    {
+        // 화면의 아래쪽 절반에서만 입력 처리
+        if (inputPosition.y > Screen.height * 0.5f) return;
+        
+        // UI용 RectTransform 가져오기
+        RectTransform rectTransform = player as RectTransform;
+        
+        // 스크린 좌표를 UI 좌표로 변환
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            rectTransform.parent as RectTransform, // Canvas RectTransform
+            inputPosition,
+            null, // UI가 Screen Space - Overlay 모드일 경우 null
+            out Vector2 localPoint
+        );
+
+        // 현재 위치에서 x값만 부드럽게 변경
+        Vector2 currentPos = rectTransform.anchoredPosition;
+        currentPos.x = Mathf.Lerp(currentPos.x, localPoint.x, moveSpeed * Time.deltaTime);
+        
+        // 위치 업데이트
+        rectTransform.anchoredPosition = currentPos;
     }
 
     public void StartSpriteSwitch(int spriteIndex)
